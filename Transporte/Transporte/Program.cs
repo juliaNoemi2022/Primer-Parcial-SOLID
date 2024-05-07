42cfa637275ff1f3cc4beea98363c25bbb527a28
Julia Avalos <julianoemi34@gmail.com>
2024-05-07 19:10:56 -0300

    armado de clases

--- Transporte/Transporte/Program.cs
+++ Transporte/Transporte/Program.cs
@@ -1,41 +1,52 @@
-﻿using System.Collections.Generic;
-using System;
-List transportes = new List();
+﻿using System;
+using System.Collections.Generic;
 
-Avion avion1 = new Avion();
-avion1._nombre = "AVION 01";
-avion1._velocidadActual = 0;
-avion1._velocidadMaxima = 100;
-avion1._alturaActual = 0;
-avion1._alturaMaxima = 100;
-transportes.Add(avion1);
-
-Avion avion2 = new Avion();
-avion2._nombre = "AVION 02";
-avion2._velocidadActual = 0;
-avion2._velocidadMaxima = 100;
-avion2._alturaActual = 0;
-avion2._alturaMaxima = 100;
-transportes.Add(avion2);
-
-Tren tren1 = new Tren();
-tren1._nombre = "TREN 01";
-tren1._velocidadActual = 0;
-tren1._velocidadMaxima = 100;
-transportes.Add(tren1);
-
-Console.WriteLine("Inicio del viaje :)");
-
-foreach (ITransporte transporte in transportes)
+namespace primer_parcial_solid_transporte
 {
-    if (transporte is Avion)
+    class Program
     {
-        transporte.Volar(10);
-    }
-    else
-    {
-        transporte.Acelerar(10);
+        static void Main(string[] args)
+        {
+
+            List<ITransporte> transportes = new List<ITransporte>();
+
+            Avion avion1 = new Avion();
+            avion1._nombre = "AVION 01";
+            avion1._velocidadActual = 0;
+            avion1._velocidadMaxima = 100;
+            avion1._alturaActual = 0;
+            avion1._alturaMaxima = 100;
+            transportes.Add(avion1);
+
+            Avion avion2 = new Avion();
+            avion2._nombre = "AVION 02";
+            avion2._velocidadActual = 0;
+            avion2._velocidadMaxima = 100;
+            avion2._alturaActual = 0;
+            avion2._alturaMaxima = 100;
+            transportes.Add(avion2);
+
+            Tren tren1 = new Tren();
+            tren1._nombre = "TREN 01";
+            tren1._velocidadActual = 0;
+            tren1._velocidadMaxima = 100;
+            transportes.Add(tren1);
+
+            Console.WriteLine("Inicio del viaje :)");
+
+            foreach (ITransporte transporte in transportes)
+            {
+                if (transporte is Avion)
+                {
+                    transporte.Volar(10);
+                }
+                else
+                {
+                    transporte.Acelerar(10);
+                }
+            }
+
+            Console.WriteLine("Fin del viaje :)");
+        }
     }
 }
-
-Console.WriteLine("Fin del viaje :)");
